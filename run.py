532e69de0fe568d1bab988a7afc5a76a51044a96
Anthoni Mathias <anthonimathias@gmail.com>
2021-08-04 10:42:23 +0100

    Add circle object

--- run.py
+++ run.py
@@ -21,4 +21,5 @@ while True:
       pygame.quit()
       exit()
   pygame.draw.rect(screen, (255,0,0), (200,300,40,50))
+  pygame.draw.circle(screen, (0,0,120), (300,260), 40)
   pygame.display.update()
