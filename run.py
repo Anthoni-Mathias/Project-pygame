17f407de49c2a9bf70ba46329295ab6ec24f7677
Anthoni Mathias <anthonimathias@gmail.com>
2021-08-04 10:26:17 +0100

    Add name to game

--- run.py
+++ run.py
@@ -10,3 +10,14 @@ Screen size for the game
 width = 640
 height = 480
 screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('title')
+
+"""
+Loop efect
+"""
+while True:
+  for event in pygame.event.get():
+    if event.type == QUIT:
+      pygame.quit()
+      exit()
+  pygame.display.update()
